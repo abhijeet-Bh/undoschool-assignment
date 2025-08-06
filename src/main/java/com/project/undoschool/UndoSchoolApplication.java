ffbbdcd121e84ded17c2d65bea46f4935063e727
abhijeet-Bh <abhijeetbhardwaj53@gmail.com>
2025-08-06 18:20:43 +0530

    feat: setup done

---
+++ src/main/java/com/project/undoschool/UndoSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.project.undoschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndoSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UndoSchoolApplication.class, args);
+	}
+
+}
